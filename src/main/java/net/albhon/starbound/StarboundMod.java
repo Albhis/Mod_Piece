12fa03c7d005879a39ffb53194165609112686df
Albhis <FlamedermanAlbhis@gmail.com>
2022-06-03 15:36:15 -0600

    Add files via upload

---
+++ src/main/java/net/albhon/starbound/StarboundMod.java
@@ -0,0 +1,41 @@
+package net.albhon.starbound;
+
+import net.albhon.starbound.block.ModBlocks;
+import net.albhon.starbound.item.ModItems;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(StarboundMod.MOD_ID)
+public class StarboundMod
+{
+    public static final String MOD_ID = "starbound";
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public StarboundMod() {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
